771992389cc70603988f4e88b07558b950b52515
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-25 12:24:42 -0600

    Update system.mjs

--- system.mjs
+++ system.mjs
@@ -7,10 +7,10 @@ Hooks.once("init", () => {
   // Register system-wide config namespace
   CONFIG["hwfwm-system"] = HWFWM_CONFIG;
 
-  // Register the Actor sheet (applies to all actor types for now)
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("hwfwm-system", HwfwmActorSheet, {
-    makeDefault: true,
-    label: "HWFWM Actor Sheet"
-  });
+ // Register the Actor sheet (applies to all actor types for now)
+Actors.unregisterSheet("core", BaseActorSheet ?? ActorSheet);
+Actors.registerSheet("hwfwm-system", HwfwmActorSheet, {
+  makeDefault: true,
+  label: "HWFWM Actor Sheet"
+});
 });
