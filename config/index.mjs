59793fe0b859e598987821920612742cbf908da8
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-25 12:00:04 -0600

    Create index.mjs

---
+++ config/index.mjs
@@ -0,0 +1,7 @@
+/**
+ * HWFWM system configuration root.
+ * Individual config domains (ranks, races, backgrounds, etc.)
+ * will be added and imported here as needed.
+ */
+
+export const HWFWM_CONFIG = {};
