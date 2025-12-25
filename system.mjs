d82484f67a9a08e23e9638b621ae2b6fb61a5437
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-25 12:00:59 -0600

    Update system.mjs

--- system.mjs
+++ system.mjs
@@ -1,3 +1,8 @@
+import { HWFWM_CONFIG } from "./config/index.mjs";
+
 Hooks.once("init", () => {
   console.log("HWFWM System | Initialized");
+
+  // Register system-wide config namespace
+  CONFIG["hwfwm-system"] = HWFWM_CONFIG;
 });
