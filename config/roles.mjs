5afaa69d1731626d0c7ba7ace7a569e08f42490d
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-25 12:15:49 -0600

    Create roles.mjs

---
+++ config/roles.mjs
@@ -0,0 +1,25 @@
+/**
+ * Role configuration (display only).
+ * Mechanical implications will be added later.
+ */
+
+export const ROLES = {
+  brawler: "Brawler",
+  defender: "Defender",
+  guardian: "Guardian",
+  skirmisher: "Skirmisher",
+  healer: "Healer",
+  striker: "Striker"
+};
+
+/**
+ * Canonical ordering for dropdowns and iteration.
+ */
+export const ROLE_ORDER = [
+  "brawler",
+  "defender",
+  "guardian",
+  "skirmisher",
+  "healer",
+  "striker"
+];
