d1def7a882a6e37be4ed1ae43ba84d0d2bf157ae
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-27 08:06:21 -0600

    Create confluence-essences.mjs

---
+++ config/confluence-essences.mjs
@@ -0,0 +1,27 @@
+/**
+ * Confluence Essence configuration
+ * Reference data only (do not mutate at runtime).
+ *
+ * Confluence essences represent hybrid / higher-order forces
+ * layered on top of a primary Essence.
+ *
+ * Display convention on sheet:
+ * Confluence: <Name>
+ */
+export const HWFWM_CONFLUENCE_ESSENCES = {
+  doom: {
+    name: "Doom"
+  },
+
+  abyssal: {
+    name: "Abyssal"
+  },
+
+  arsenal: {
+    name: "Arsenal"
+  },
+
+  undeath: {
+    name: "Undeath"
+  }
+};
