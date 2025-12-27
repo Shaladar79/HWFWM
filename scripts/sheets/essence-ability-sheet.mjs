379ff7808bfdb73b194773844fb6492add7fd64b
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-27 09:08:41 -0600

    Update essence-ability-sheet.mjs

--- scripts/sheets/essence-ability-sheet.mjs
+++ scripts/sheets/essence-ability-sheet.mjs
@@ -10,4 +10,18 @@ export class HwfwmEssenceAbilitySheet extends ItemSheet {
   get template() {
     return "systems/hwfwm-system/templates/item/essence-ability-sheet.hbs";
   }
+
+  async getData(options = {}) {
+    const data = await super.getData(options);
+
+    // Make sure templates can reliably use `system.*`
+    data.system = this.item?.system ?? {};
+
+    // Inject config catalogs for dropdowns
+    const cfg = CONFIG["hwfwm-system"] ?? {};
+    data.essenceCatalog = cfg.essenceCatalog ?? {};
+    data.confluenceEssenceCatalog = cfg.confluenceEssenceCatalog ?? {};
+
+    return data;
+  }
 }
